9aa52f743772d2bd2e99283e36ac049e289308fb
Carlos Diaz-Padron <carlosdiazpadron@gmail.com>
2021-08-11 11:33:46 -0400

    more setup

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { registerRootComponent } from 'expo';
+import firebase from 'firebase';
 
-export default function App() {
+const firebaseConfig = {
+  apiKey: "AIzaSyA7fqVE1VoM81x3bJfKOaH7tK0Ka8b9FvI",
+  authDomain: "drop---avatars.firebaseapp.com",
+  projectId: "drop---avatars",
+  storageBucket: "drop---avatars.appspot.com",
+  messagingSenderId: "297448976018",
+  appId: "1:297448976018:web:5c49b68e6711ef06ae804b",
+  measurementId: "G-ZMSMGXMDGX"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+if (process.env.NODE_ENV === 'development') {
+  firebase.functions().useEmulator('localhost', 5001);
+  firebase.auth().useEmulator('http://localhost:9099');
+}
+
+function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -17,3 +36,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default registerRootComponent(App);
+
