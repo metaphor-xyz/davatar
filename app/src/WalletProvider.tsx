5b6f68b5cf5ede7c38c357a4a387e23f39787809
Carlos Diaz-Padron <carlosdiazpadron@gmail.com>
2021-08-24 15:05:28 -0400

    load nfts from OpenSea API

--- app/src/WalletProvider.tsx
+++ app/src/WalletProvider.tsx
@@ -6,11 +6,22 @@ import { Platform } from 'react-native';
 import Web3 from 'web3';
 import Web3Modal from 'web3modal';
 
+export interface OpenSeaNFT {
+  id: number;
+  name: string;
+  description: string;
+  image_thumbnail_url: string;
+  image_preview_url: string;
+  image_url: string;
+}
+
 export interface Context {
   wallet: Web3 | null;
   address: string | null;
   connect: () => Promise<Web3 | null>;
   disconnect: () => void;
+  connecting: boolean;
+  nfts: OpenSeaNFT[];
 }
 
 const WalletContext = createContext<Context>(null!);
@@ -21,6 +32,8 @@ function WalletProvider(props: React.PropsWithChildren<Record<string, never>>) {
   const [wallet, setWallet] = useState<Web3 | null>(null);
   const [address, setAddress] = useState<string | null>(null);
   const connector = useWalletConnect();
+  const [connecting, setConnecting] = useState(false);
+  const [nfts, setNfts] = useState([]);
 
   useEffect(() => {
     if (Platform.OS === 'web') {
@@ -41,38 +54,53 @@ function WalletProvider(props: React.PropsWithChildren<Record<string, never>>) {
   const connect = useCallback(async () => {
     let newWallet: Web3 | null = null;
 
-    if (Platform.OS === 'web') {
-      if (web3Modal) {
-        const provider = await web3Modal.connect();
-        newWallet = new Web3(provider);
-      }
-    } else {
-      if (!connector.connected) {
-        await connector.connect();
-      }
-      const provider = new WalletConnectProvider({
-        connector,
-        infuraId: 'e6e57d41c8b2411ea434bf96efe69f08',
-      });
+    setConnecting(true);
 
-      await provider.enable();
+    try {
+      if (Platform.OS === 'web') {
+        if (web3Modal) {
+          const provider = await web3Modal.connect();
+          newWallet = new Web3(provider);
+        }
+      } else {
+        if (!connector.connected) {
+          await connector.connect();
+        }
+        const provider = new WalletConnectProvider({
+          connector,
+          infuraId: 'e6e57d41c8b2411ea434bf96efe69f08',
+        });
 
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      newWallet = new Web3(provider as any);
+        await provider.enable();
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        newWallet = new Web3(provider as any);
+      }
+
+      if (newWallet) {
+        const accounts = await newWallet.eth.getAccounts();
+
+        if (accounts.length < 1) {
+          return null;
+        }
+
+        // Load NFT information from OpenSea
+        fetch(`https://api.opensea.io/api/v1/assets?owner=${accounts[0]}&order_direction=desc&offset=0&limit=50`)
+          .then(res => res.json())
+          .then(data => {
+            if (data && data.assets) {
+              setNfts(data.assets);
+            }
+          });
+
+        setAddress(accounts[0]);
+        setWallet(newWallet);
+      }
+
+      return newWallet;
+    } finally {
+      setConnecting(false);
     }
-
-    if (newWallet) {
-      const accounts = await newWallet.eth.getAccounts();
-
-      if (accounts.length < 1) {
-        return null;
-      }
-
-      setAddress(accounts[0]);
-      setWallet(newWallet);
-    }
-
-    return newWallet;
   }, [connector]);
 
   const disconnect = useCallback(() => {
@@ -82,19 +110,23 @@ function WalletProvider(props: React.PropsWithChildren<Record<string, never>>) {
   }, [connector]);
 
   useEffect(() => {
-    if (Platform.OS === 'web' && web3Modal) {
-      if (web3Modal.cachedProvider) {
-        connect();
-      }
-    } else {
-      if (connector.connected) {
-        connect();
+    if (!wallet && !connecting) {
+      if (Platform.OS === 'web' && web3Modal) {
+        if (web3Modal.cachedProvider) {
+          connect();
+        }
+      } else {
+        if (connector.connected) {
+          connect();
+        }
       }
     }
-  }, [connect, connector]);
+  }, [wallet, connecting, connect, connector]);
 
   return (
-    <WalletContext.Provider value={{ wallet, address, connect, disconnect }}>{props.children}</WalletContext.Provider>
+    <WalletContext.Provider value={{ wallet, address, connect, disconnect, connecting, nfts }}>
+      {props.children}
+    </WalletContext.Provider>
   );
 }
 
