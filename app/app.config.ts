3753d3f294ce1a56f1ec8d074f48d3a0313a7537
Gail Wilson <gail.de.wilson@gmail.com>
2021-08-25 22:03:38 -0400

    Add favicon and description

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,10 +1,16 @@
 import { ExpoConfig, ConfigContext } from '@expo/config';
 
+// eslint-disable-next-line
 export default ({ config }: ConfigContext): ExpoConfig => {
   return {
     ...config,
-    name: 'DAOvatar',
-    slug: 'daovatar',
+    name: 'davatar',
+    slug: 'davatar',
+    web: {
+      favicon: './src/assets/alien-invader.png',
+      name: 'davatar',
+      description: 'One decentralized avatar for everything Web3.',
+    },
     entryPoint: './src/App.tsx',
   };
 };
